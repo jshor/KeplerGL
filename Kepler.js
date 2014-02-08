ba593b853a8a1509dc130e5c10d8e3f853d2b4c7
Joshua Shor <jpshor@gmail.com>
2014-02-08 18:27:52 -0500

    renamed directories, removed requirejs redundancy

--- Kepler.js
+++ Kepler.js
@@ -2,48 +2,39 @@ require.config({
 	baseUrl: "app/",
     paths: {
 		"app": "./app",
-		"threejs" : "../lib/three.min",
-		"threejs/OrbitControls": "../lib/OrbitControls",
-		"threejs/GridHelper": "../lib/GridHelper",
-		"threejs/OBJLoader": "../lib/OBJLoader",
+		"three": "../lib/three",
+		"threejs": "../lib/three/threejs",
 		"objects/HeliocentricObject": "objects/HeliocentricObject",
 		"interface/DialogWindow": "interface/DialogWindow",
-		jquery : "../lib/jquery",
-        "jquery.nouislider.min": "../lib/jquery.nouislider.min"
+		jquery : "../lib/jquery/jquery",
+        "jquery.nouislider.min": "../lib/jquery/jquery.nouislider.min"
     },
     shim: {
         "jquery.nouislider.min": {
 			deps: ["jquery"],
             exports: '$'
 		},
-		"threejs/OrbitControls": {
+		"three/OrbitControls": {
 			deps: ["threejs"]
 		},
-		"objects/HeliocentricObject": {
-			deps: ["interface/DialogWindow"]
-		},
-		"threejs/GridHelper": {
+		"three/GridHelper": {
 			deps: ["threejs"]
 		},
-		"threejs/OBJLoader": {
+		"three/OBJLoader": {
 			deps: ["threejs"]
 		}
     },
-	priority : ['jquery', 'threejs']
+	priority : ['jquery', 'three']
 });
 
-require([
+define([
 	"jquery",
-	"jquery.nouislider.min",
-	"threejs",
-	"objects/HeliocentricObject",
 	"objects/SolarSystem",
-	"interface/Controls",
 	"physics/Astrodynamics",
-	"interface/DialogWindow",
+	"interface/Controls",
 	"Scene",
 ],
-function ($) {
+function($, SolarSystem, Astrodynamics, Controls, Scene) {
     // TBD: add in a loading screen, and check for webgl
 	// if no webgl is present, show the user an info screen
     var Scene 		= require('Scene');
@@ -53,8 +44,7 @@ function ($) {
     var DialogWindow 	= require('interface/DialogWindow');
 
     var glScene 			= new Scene();
-	var Astrodynamics		= new Astrodynamics();
-	var solarSystemScene 	= new SolarSystem(glScene, Astrodynamics);
+	var solarSystemScene 	= new SolarSystem(glScene);
 	
 	/* position solar system objects and start the clock */
 	glScene.setSolarSystemScene(solarSystemScene);
