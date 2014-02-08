b3f4f68ec2b1b956e1ce471d12bdc013afcf4b1e
Joshua Shor <jpshor@gmail.com>
2014-02-08 11:57:01 -0500

    fixed zoom functions, removed unused functions, fixed control loop, fixed window resize event, set perspective events, added grid helpers

--- app/Scene.js
+++ app/Scene.js
@@ -9,44 +9,53 @@ function ($, Controls) {
 		var OrbitControls 	= require('threejs/OrbitControls');
 		var Controls		= require('interface/Controls');
 		
-		/* scale of km per webGL unit */
+		// scale of km per webGL unit
 		this.timeSpeedScale = 10;
 		this.scaleConstant = 100000000000;
+
+		// initial conditions for scene
+		this.zoomDestination = 100;
+		this.zoomLevel = 1;
+		this.allowZoom = false;
+		this.isZoomUpdating = false;
 		this.paused = true;
 		
-		/* initialize controls */
+		// initialize user interface controls 
 		this.interfaceControls = new Controls();
 		
-		/* initialize the WebGL renderer */
-		this.renderer	= new THREE.WebGLRenderer({ antialias: true, alpha: true });//
-		this.camera		= new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.0001, 5);
+		// initialize the WebGL renderer
+		this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+		this.camera	= new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.01, 1000);
+		var width = window.innerWidth;
+		var height = window.innerHeight;
 
-		/* camera controls */
-		this.camera.position.set(0.05, 0.05, 0.05);
-		this.controls	= new THREE.OrbitControls(this.camera);		
-		this.controls.minDistance = 0.0002;
-		this.controls.maxDistance = 0.1;
+		// camera controls
+		this.camera.position.set(10, 10, 10);
+		this.controls = new THREE.OrbitControls(this.camera);		
+		this.controls.minDistance = 0;
+		this.controls.maxDistance = 10;
 		
-		this.startDate	= new Date().getTime();
+		this.startDate = new Date().getTime();
 		
-		/* initialize the scene's controls */
+		// initialize the scene's controls
 		this.interfaceControls.setControls(this);
 		this.interfaceControls.correctZoomScrollHeight();
 		
-		/* set size of renderer and camera's position */
+		// set size of renderer and camera's position
 		this.renderer.setSize(window.innerWidth, window.innerHeight);
 		this.renderer.physicallyBasedShading = true;
+		this.renderer.autoClear = false; // To allow render overlay on top of sprited sphere
 		document.body.appendChild(this.renderer.domElement);
 		
-		/* initialize time (t=0 at init) */
+		// initialize time (t=0 at init)
 		this.time = 0;
 		
-		/* camera rests on a plane which can repositioned (useful for tours) */
+		// camera rests on a plane which can repositioned (useful for tours)
 		this.camerapivot = new THREE.Object3D();
-		this.perspective = "Saturn";/////////////////////////////////////////////////////////////////////////////////////
+		this.perspective = "Sun";/////////////////////////////////////////////////////////////////////////////////////
 		this.camerapivot.add(this.camera);
 		
-		/* initialize the scene */
+		// initialize the scene 
 		this.glScene = new THREE.Scene();
 		this.glScene.add(this.camerapivot);
 	//	this.glScene.add( new THREE.AxisHelper( 1 ) );
@@ -58,8 +67,67 @@ function ($, Controls) {
 
 		gridHelper.position = new THREE.Vector3( 0 , 0 , 0 );
 		gridHelper.rotation = new THREE.Euler( 0 , 0, 0 );
+		
+		var skyGeometry = new THREE.SphereGeometry( 1, 32, 32);	
+		this.skyMaterial = new THREE.MeshLambertMaterial({
+			map: THREE.ImageUtils.loadTexture("app/textures/milky.jpg"),
+			side: THREE.BackSide,
+			color:0x000000, ambient:0x000000, emissive:0x808080});
 
-		//this.glScene.add( gridHelper );
+	this.cameraOrtho = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.0001, 1000);
+	this.cameraOrtho.position.set(0.05, 0.05, 0.05);
+		this.controlsOrtho	= new THREE.OrbitControls(this.cameraOrtho);		
+		this.controlsOrtho.minDistance = 0.0002;
+		this.controlsOrtho.maxDistance = 0.1;
+	this.sceneOrtho = new THREE.Scene();
+	this.sceneOrtho.add(this.cameraOrtho);
+	
+	this.skyBox = new THREE.Mesh( skyGeometry, this.skyMaterial );
+	//	this.skyBox.scale.set(-1, 1, 1);
+//	this.skyBox.rotation.set(0, .3, -.9);
+      this.skyBox.rotation.z = Math.PI*25/32;  // get the milky way in the right place
+      this.skyBox.rotation.x = Math.PI/11;
+//	this.skyBox.renderDepth = 1000.0;
+	
+	this.sceneOrtho.add( this.skyBox );
+	
+	
+//		this.glScene.add( gridHelper );
+		this.hoverLabel = "";
+	};
+	
+	Scene.prototype.setMouseHover = function(label, over) {
+		this.hoverLabel = (over ? label : "");
+	};
+	
+	Scene.prototype.setPerspective = function(objectName, callback, objectParentName) {
+		// set the object that the camera is focused on
+		this.perspective = objectName;
+		this.perspectiveParent = (objectParentName != undefined ? objectParentName : "");
+		this.zoomLevel = 100-this.camera.fov*100;
+		this.zoomDestination = 100;
+		this.allowZoom = true;
+		this.callback = callback;
+	};
+	
+	Scene.prototype.getPerspective = function(getParent) {
+		// get the object that the camera is focused on, return parent if getParent=
+		return (getParent ? this.perspectiveParent : this.perspective);
+	};
+	
+	Scene.prototype.updateCameraFocus = function(vector) {
+		// set the vector that the camera plane is centered at
+		this.camerapivot.position = vector;
+	};
+	
+	Scene.prototype.setSolarSystemScene = function(solarSystemScene) {
+		this.solarSystemScene = solarSystemScene;
+		this.renderer.sortObjects = false;
+	};
+	
+	Scene.prototype.changeZoom = function(zoomChange) {
+		this.zoomDestination = 100-zoomChange;
+		this.allowZoom = true;
 	};
 	
 	Scene.prototype.add = function(object) {
@@ -79,7 +147,7 @@ function ($, Controls) {
 	};
 	
 	Scene.prototype.getSpeedScale = function() {
-		return this.timeSpeedScale / this.scaleConstant; // TBD: modify so users can change this
+		return this.timeSpeedScale / this.scaleConstant;
 	};
 
 	Scene.prototype.scaleSpeed = function(speed, timeSpeed) {
@@ -89,81 +157,59 @@ function ($, Controls) {
 	};
 	
 	Scene.prototype.getScaleConstant = function() {
+		// scaling factor for scene
 		return this.scaleConstant;
-	};
-	
-	Scene.prototype.cameraFocus = function(objectName) {
-		/* get or set the object that the camera is focused on */
-		if(objectName != undefined)
-			this.perspective = objectName;
-		
-		return this.perspective;
-	};
-	
-	Scene.prototype.updateCameraFocus = function(vector) {
-		/* set the vector that the camera plane is centered at */
-		this.camerapivot.position = vector;
-	};
-	
-	Scene.prototype.setSolarSystemScene = function(solarSystemScene) {
-		this.solarSystemScene = solarSystemScene;
-		this.renderer.sortObjects = false;
-	};
-	
-	Scene.prototype.changeZoom = function(zoom) {
-		var cam = this.camera.position;
-		var tar = this.controls.target;
-		var dist = Math.sqrt(Math.pow(Math.abs(cam.x-tar.x), 2)+Math.pow(Math.abs(cam.y-tar.y), 2)+Math.pow(Math.abs(cam.z-tar.z), 2));
-		var distPercent = ((dist / (this.controls.maxDistance-this.controls.minDistance))*100);
-		
-		this.controls.dollyIn(dist*Math.abs(100-zoom));
-	};
-	
-	Scene.prototype.getScaleConstant = function() {
-		return 100000000000;
 	};
 	
     Scene.prototype.animate = function() {
 		this.camera.updateProjectionMatrix();
 		this.glScene.updateMatrixWorld();
+		this.renderer.render(this.sceneOrtho, this.cameraOrtho);
 		this.renderer.render(this.glScene, this.camera);
 		
-		/* change zoom of camera */
-		//this.camera.fov = $("#zoomScroll").val();
+		// update the camera frustum
+		this.camera.fov = this.controls.getRadius() / (this.controls.maxDistance-this.controls.minDistance);
 		
-		// distance between camera position and target position
-		// should be in Math prototype class instead of scene prototype class.
-		if(this.controls.target != undefined) {
-			var cam = this.camera.position;
-			var tar = this.controls.target;
-			var dist = Math.sqrt(Math.pow(Math.abs(cam.x-tar.x), 2)+Math.pow(Math.abs(cam.y-tar.y), 2)+Math.pow(Math.abs(cam.z-tar.z), 2));
-			var distPercent = ((dist / (this.controls.maxDistance-this.controls.minDistance))*100);
-			this.camera.fov = distPercent;
-			
-			this.interfaceControls.updateZoom(distPercent);
+		if(!this.isZoomUpdating)
+			this.interfaceControls.setZoom(this.camera.fov*100);
 
-			$("#tourLink").html(this.camera.fov);
-		}
-	//	this.solarSystemScene.getPlanet().zoomIn();
+		// show/hide the title if the camera is relatively close (25% radius) to the object
+		if(this.camera.fov <= 0.25)
+			this.interfaceControls.toggleTitle(true);
+		else
+			this.interfaceControls.toggleTitle(false);
+		
 		this.camera.updateProjectionMatrix();
-	//	console.log(this.camera.fov);
 		this.controls.update();
-	//	$("#zoomScroll").val(level, true); // change zoom level of interface scroll
+		this.controlsOrtho.update();
+		
 		requestAnimationFrame(this.animate.bind(this));
 	};
-	
-	Scene.prototype.shiftLeft = function() { // TBD: this method should be in Controls.js
-		$("canvas").width($("canvas").width()+600);
-		$("canvas").css({ marginTop: -150 });
-		$("canvas").css({ marginLeft: -600 });
-		$("#dialog").show();
-		$("#dialogWindow heading").html("Mars");
+
+	Scene.prototype.updateZoom = function() {
+		// update to the requested zoom level
+		if(this.allowZoom) {
+			if(this.zoomLevel+1 <= this.zoomDestination) {
+				this.controls.dolly(1-this.zoomLevel/100);
+				this.zoomLevel += 2;
+			} else if(this.zoomLevel-1 > this.zoomDestination) {
+				this.controls.dolly(1-this.zoomLevel/100);
+				this.zoomLevel -= 2;
+			} else {
+				this.allowZoom = false;
+				
+				if(this.callback)
+					this.callback();
+				this.callback = null;
+			}
+		}
 	};
 	
 	Scene.prototype.clockStart = function() {
-		var self 		= this;
+		var self = this;
 		var currentDate = new Date();
 		var monthName = ["January", "February", "March", "April", "May", "June", "July", "Augst", "September", "October", "November", "December"];
+		var c = 0;
 		
 		// TBD: bug in Chrome: use setTimeout() instead, put calendar fns in Time.js
 		this.clock 	= setInterval(function() {
@@ -180,7 +226,9 @@ function ($, Controls) {
 					+ (currentDate.getSeconds() < 10 ? "0" : "") + currentDate.getSeconds() + " "
 					+ currentDate.getTimezoneOffset()
 			);
-		//	$("#julianDateTime").html(newtime);
+			
+			self.updateZoom();
+			
 			if(self.solarSystemScene && !self.paused) {
 				self.paused = self.solarSystemScene.updatePositions(self.paused);
 			}
@@ -195,11 +243,8 @@ function ($, Controls) {
 		return this.time;
 	};
 	
-	Scene.prototype.showInfo = function() {
-		
-	};
-	
 	Scene.prototype.windowResize = function() {
+		this.interfaceControls.correctZoomScrollHeight();
 		this.renderer.setSize(window.innerWidth, window.innerHeight);
 		this.camera.aspect = window.innerWidth / window.innerHeight;
 		this.camera.updateProjectionMatrix();
