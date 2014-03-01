1330cdf44865d795ae169d16597eabff7eef15ac
Joshua Shor <jpshor@gmail.com>
2014-03-01 10:41:59 -0500

    added exception for undefined vectors

--- app/physics/Vector.js
+++ app/physics/Vector.js
@@ -1,10 +1,14 @@
 var Vectors = {
 	magnitude: function(v) {
-		if(!isNaN(v.x) && !isNaN(v.y)) {
-			var sum = Math.pow(v.x, 2) + Math.pow(v.y, 2);
-			sum += (!isNaN(v.z) ? v.z : 0);
-			
-			return Math.sqrt(sum);
+		if(v != undefined) {
+			if(!isNaN(v.x) && !isNaN(v.y)) {
+				var sum = Math.pow(v.x, 2) + Math.pow(v.y, 2);
+				sum += (!isNaN(v.z) ? v.z : 0);
+				
+				return Math.sqrt(sum);
+			} else {
+				return 0;
+			}
 		} else {
 			return 0;
 		}
