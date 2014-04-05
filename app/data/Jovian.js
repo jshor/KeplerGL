1c7bf37c602de16a163fb6b0677d9e8b3b8cacf6
jshor <truthsofwolf@gmail.com>
2014-04-04 22:47:53 -0400

    added argument of periapsis, longitude of asc. node and textures for galilean moons

--- app/data/Jovian.js
+++ app/data/Jovian.js
@@ -7,19 +7,16 @@ var Jupiter = {
 	semimajor: 778547200,
 	semiminor: 777620566,
 	radius: 69911,
+	rotation: 15.0411, // in arcseconds
 	inclination: 1.31,
 	argPeriapsis: 275.066,
 	lastPeriapsis: 1300492800, // UNIX time
 	nextPeriapsis: 1674828800, // UNIX time
 	eccentricity: 0.048775,
 	longAscNode: 100.492,
-	texture: "Jupiter.jpg",
-	rings: [
-		{
-			innerRadius: 6630,
-			outerRadius: 120700
-		}
-	],
+	texture: {
+		map: "Jupiter.jpg"
+	},
 	satellites: [
 		{
 			objectType: "Moon",
@@ -30,11 +27,14 @@ var Jupiter = {
 			semiminor: 671010,
 			radius: 1565,
 			inclination: 0.464,
-	argPeriapsis: 275.066,
-			lastPeriapsis: 1406689200,
-			nextPeriapsis: 1406992500,
+			argPeriapsis: 5.718926517499346E+01,
+			lastPeriapsis: 1136437860,
+			nextPeriapsis: 1136486160,
 			eccentricity: 0.0101,
-	longAscNode: 100.492,
+			longAscNode: 3.487875720612225E+02,
+			texture: {
+				map: "Europa.jpg"
+			}
 		},
 		{
 			objectType: "Moon",
@@ -45,11 +45,14 @@ var Jupiter = {
 			semiminor: 1070410,
 			radius: 2634.1,
 			inclination: 0.204,
-	argPeriapsis: 275.066,
-			lastPeriapsis: 1406689200,
-			nextPeriapsis: 1406992500,
+			argPeriapsis: 3.055548024810087E+02,
+			lastPeriapsis: 1136341380,
+			nextPeriapsis: 1137201240,
 			eccentricity: 0.0011,
-	longAscNode: 100.492,
+			longAscNode: 3.421450259947836E+02,
+			texture: {
+				map: "Ganymede.jpg"
+			}
 		},
 		{
 			objectType: "Moon",
@@ -60,11 +63,14 @@ var Jupiter = {
 			semiminor: 1882605,
 			radius: 2410.3,
 			inclination: 0.204,
-	argPeriapsis: 275.066,
-			lastPeriapsis: 1406689200,
-			nextPeriapsis: 1406992500,
+			argPeriapsis: 1.910576843073536E+01,
+			lastPeriapsis: 1136710800,
+			nextPeriapsis: 1138161600,
 			eccentricity: 0.0074,
-	longAscNode: 100.492,
+			longAscNode: 3.375938901353522E+02,
+			texture: {
+				map: "Callisto.jpg"
+			}
 		},
 		{
 			objectType: "Moon",
@@ -76,12 +82,14 @@ var Jupiter = {
 			semiminor: 421692,
 			radius: 13642.66,
 			inclination: 0.05,
-	argPeriapsis: 286.537,
-			lastPeriapsis: 1406689200,
-			nextPeriapsis: 1406992500,
+			argPeriapsis: 2.502432229764357E+02,
+			lastPeriapsis: 1136183760,
+			nextPeriapsis: 1136336100,
 			eccentricity: 0.0041,
-	longAscNode: 49.562,
-			texture: "Jupiter.jpg"
+			longAscNode: 3.378889108567996E+02,
+			texture: {
+				map: "Io.jpg"
+			}
 		}
 	],
 	atmosphere: 0x988774
